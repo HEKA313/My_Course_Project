5a42ff3701fad1b77a48ec06d28e56305efe9b0a
HEKA313 <52586811+HEKA313@users.noreply.github.com>
2020-12-03 00:22:26 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/My_Course_Project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/My_Course_Project.dir/main.c.o"
+  "My_Course_Project.exe"
+  "My_Course_Project.pdb"
+  "libMy_Course_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/My_Course_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
